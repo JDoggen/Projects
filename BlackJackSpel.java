46fe043934fbdbe1980fde976001d24f2ccfc6c9
unknown <jpdoggen@gmail.com>
2018-08-20 15:39:05 +0200

    Eerste commit

---
+++ BlackJackSpel.java
@@ -0,0 +1,7 @@
+class BlackJackSpel{
+	public static void main(String[] args){
+		System.out.println("Blackjack is begonnen");
+	}
+
+
+}
---
+++ Demonstratie.java
@@ -0,0 +1,5 @@
+class Demonstratie{
+  public static void main(String[] args){
+    System.out.println("Hallo Wereld");
+  }
+}
